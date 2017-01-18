39ca700fec3671d76e479f079ff30a50f5bf41cd
Tim Taylor <ttaylor@clarion.edu>
2017-01-17 19:23:30 -0500

    ex3 using math symbols for calculations and boolean expressions

--- LRTHW/ex3.rb
+++ LRTHW/ex3.rb
@@ -1,23 +1,23 @@
 puts "I will now count my chickens:"
 
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25 + 30 / 6}" # line 3: the comp will run the division 1st + 25 = 30
+puts "Roosters #{100 - 25 * 3 % 4}" # line 4: multiply 1st so 25x3=75%(mod op) 4 remander 3 from 100 = 97...don't ask me why???
 
-puts "Now I will count the eggs:"
+puts "Now I will count the eggs:" # string statement
 
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # looks like 1 + .08 - .25 + 6 = 6.8 (7)
 
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3 + 2 < 5 - 7?" # boolean statement and 5 is not less than -2
 
-puts 3 + 2 < 5 -7
+puts 3 + 2 < 5 -7 # the output from the above statement
 
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3 + 2? #{3 + 2}" # the string statment with the equation 5
+puts "What is 5 - 7? #{5 - 7}" # the string statement with the equation -2
 
-puts "Oh, that's why it's false."
+puts "Oh, that's why it's false." # string statement
 
-puts "How about some more."
+puts "How about some more." # string statement
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it greater? #{5 > -2}" # string statement with boolean statement that is true
+puts "Is it greater or equal? #{5 >= -2}" # string statement with boolean statement that is true
+puts "Is it less or equal? #{5 <= -2}" # string statement with boolean statement that is false
