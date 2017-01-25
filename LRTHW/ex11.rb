53876fd7535d3a867bd0a5bc4a420a4d84082cde
Tim Taylor <ttaylor@clarion.edu>
2017-01-25 12:19:43 -0500

    Ex11 with some added practice in calculating age from input by converting a string to an integer

--- LRTHW/ex11.rb
+++ LRTHW/ex11.rb
@@ -8,3 +8,10 @@ weight = gets.chomp
 puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
 
 # use print instead of puts to print the string without a \n (newline) printed and the prompt stops right where the user should enter the answer.
+
+puts "What is your name? "
+name = gets.chomp
+puts "How old are you? "
+age = gets.chomp.to_i #.to_i converts the string to an integer for the calculation
+year = (2017 - age)
+puts "So, you were born in #{year}."
--- LRTHW/ex9.rb
+++ LRTHW/ex9.rb
@@ -5,8 +5,8 @@ days = "Mon Tue Wed Thu Fri Sat Sun"
 # assigning the variable 'months' with a string of months and separating them with the \n ("new line") that prints them on separate lines. I had to remove each component to be sure.
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 # printing a string with an embedded string on separate lines (puts vs. print)
-puts "here are the days: #{days}"
-puts "here are the months: #{months}"
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
 # printing (on separate lines = puts vs print) using %q (which stands for double quotes) so it acts like a string. The open bracket allows you to put as many lines as you want until the close bracket exits the execution of the string.
 puts %q{
 There's something going on here.
