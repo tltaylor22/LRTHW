aba599e0922465780911277d9a272da56d0aad36
Tim Taylor <ttaylor@clarion.edu>
2017-01-25 11:04:46 -0500

    LRTHW ex10 working with excape characters

---
+++ LRTHW/ex10.rb
@@ -0,0 +1,36 @@
+puts "I am 6'2\" tall."
+puts 'I am 6\'2" tall.'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* cat food
+\t*Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+#=>I am 6'2" tall.
+#=>I am 6'2" tall.
+#=>        I'm tabb
+#=>I'm split
+#=>on a line.
+#=>I'm \ a \ cat.
+
+#=>I'll do a list:
+#=>       * cat fo
+#=>        *Fishies
+#=>        * Catnip
+#=>        * Grass
+
+cat = 'like to eat fish that are 1\'4" long'
+days = "Mon, Tues, and Wed."
+puts "Cats #{cat} on #{days}"
+#=> Cats like to eat fish that are 1'4" long on Mon, Tues, and Wed.
