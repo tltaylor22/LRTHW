225bb6689a9640cf1f5465e9a510d76c34ce1ea3
Tim Taylor <ttaylor@clarion.edu>
2017-02-05 18:15:18 -0500

    trying to make sure there are only numbers in the number or an xX at the end. My test number has a special character so it should return false, but it returns true...AHHHH!

--- isbn/isbn.rb
+++ isbn/isbn.rb
@@ -35,14 +35,21 @@ def check_for_x_last(isbn_num)
 end	
 
 def check_for_symbol(isbn_num)
-	if isbn_num == /\D/ # checking to see if there are symbols in the string. \D is the same as [^0-9] (everything except digits) - Not sure if I need to check the 9 spot using the tilde or not????
+	if isbn_num =~ /\D/ # checking to see if there are symbols in the string. \D is the same as [^0-9] (everything except digits) - Not sure if I need to check the 9 spot using the tilde or not????
 		true
 	else
 		false
 	end
 end	
 
-def isbn_array(isbn_num)
-	isbn_array = isbn_num.split(//) # splits the string to create an array of individual numbers ["1", "2", etc]
-
+def check_valid_number_or_X_position(isbn_num)
+	if isbn_num[0..9].match(/[0-9]/) or isbn_num[-1].match(/[0-9 x X]/)
+		true
+	else
+		false
+	end
 end	
+# def isbn_array(isbn_num)
+# 	isbn_array = isbn_num.split(//) # splits the string to create an array of individual numbers ["1", "2", etc]
+
+# end	
--- isbn/test_isbn.rb
+++ isbn/test_isbn.rb
@@ -33,24 +33,39 @@ class TestISBN < Minitest::Test
 		assert_equal('1234567890', isbn_remove_spaces_dashes(isbn_num))
 	end
 
-	def test_check_for_letters
+	def test_check_for_letters_false
 		isbn_num = '123m456D7890'
 		assert_equal(false, check_for_letters(isbn_num))
 	end
 
-	def test_if_x_must_be_at_the_end
+	def test_if_x_must_be_at_the_end_true
 		isbn_num = '1234567890x'
 		assert_equal(true, check_for_x_last(isbn_num))
 	end
 
-	def test_check_for_symbol
+	def test_check_for_symbol_true
 		isbn_num = '123;456$7890'
+		assert_equal(true, check_for_symbol(isbn_num))
+	end
+
+	def test_check_for_symbol_false
+		isbn_num = '1234567890'
 		assert_equal(false, check_for_symbol(isbn_num))
 	end
 
-	def test_isbn_is_array_of_individual_numbers
-		isbn_num = '123456789'
-		assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9"], isbn_array(isbn_num))
+	def test_valid_number_or_X_position_true
+		isbn_num = '123456789X'
+		assert_equal(true, check_valid_number_or_X_position(isbn_num))
 	end
+
+	def test_valid_number_or_X_position_false
+		isbn_num = '1234$6789X'
+		assert_equal(false, check_valid_number_or_X_position(isbn_num))
+	end
+
+	# def test_isbn_is_array_of_individual_numbers
+	# 	isbn_num = '1234567890'
+	# 	assert_equal(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], isbn_array(isbn_num))
+	# end
 
 end	
